3d2dd9ad7d3cb8abe368da26ccc4b7e241f7c0ac
Leonid Shishkin <lshishkin150586@gmail.com>
2019-04-28 18:48:39 +0500

    project
    complite

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
-import React from "react"
+import React, { Component } from "react"
 import ReactDOM from "react-dom"
 
 import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox Leonid</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      val1: 0,
+      val2: 0
+    }
+    this.inputChange = this.inputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  inputChange(event) {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    const val1 = Number(this.state.val1)
+    const val2 = Number(this.state.val2)
+    const sum = (val1 + val2)
+      .toFixed(10)
+      .toString()
+      .replace(/0*$/, "")
+    alert(sum)
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="App">
+        <label>
+          Первое число
+          <input
+            id="val1"
+            value={this.state.val1}
+            onChange={this.inputChange}
+          />
+        </label>
+        <label>
+          Второе число
+          <input
+            id="val2"
+            value={this.state.val2}
+            onChange={this.inputChange}
+          />
+        </label>
+        <button>Сложить</button>
+      </form>
+    )
+  }
 }
 
 const rootElement = document.getElementById("root")
