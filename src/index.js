7d237a77844ffa73470f6bd6797feb17b54622a2
Leonid Shishkin <lshishkin150586@gmail.com>
2019-04-28 17:37:51 +0500

    h1
    change h1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css"
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox MF</h1>
+      <h1>Hello CodeSandbox Leonid</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   )
